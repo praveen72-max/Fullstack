ed17fd9edae3becb196ef83ad4dde090f78de9ab
praveen72-max <your-github-praveen72.v@gmail.com>
2025-10-31 12:52:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,30 +2,80 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Security group for SSH + HTTP
 resource "aws_security_group" "allow_http_ssh" {
-  name = "allow_http_ssh"
-  ingress = [
-    { from_port=22, to_port=22, protocol="tcp", cidr_blocks=["0.0.0.0/0"] },
-    { from_port=80, to_port=80, protocol="tcp", cidr_blocks=["0.0.0.0/0"] }
-  ]
-  egress = [{ from_port=0, to_port=0, protocol="-1", cidr_blocks=["0.0.0.0/0"] }]
+  name        = "allow_http_ssh"
+  description = "Allow SSH and HTTP access"
+
+  ingress {
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }
+
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }
+
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }
 }
 
+# Get latest Ubuntu 20.04 AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = ["099720109477"]
+  owners      = ["099720109477"]
+
   filter {
-    name = "name"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 }
 
+# Launch EC2 instance with Docker + App setup
 resource "aws_instance" "app" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  key_name      = var.ssh_key_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.ssh_key_name
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
 
-  user_data = templatefile("user_data.sh.tpl", { repo = "https://github.com/praveen72-max/Fullstack.git" })
-  tags = { Name = "fullstack-demo" }
+  user_data = templatefile(
+    "user_data.sh.tpl",
+    { repo = "https://github.com/praveen72-max/Fullstack.git" }
+  )
+
+  tags = {
+    Name = "fullstack-demo"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.app.public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.app.public_dns
 }
